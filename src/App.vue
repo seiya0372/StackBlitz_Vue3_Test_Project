b15c28a5c91459e4ec76585e41ccc05749c8dbfb
seiya0372 <91925490+seiya0372@users.noreply.github.com>
2023-01-27 13:33:04 +0900

    1 file modified

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="https://vuejs.org/images/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js Apcp"/>
+    <HelloWorld msg="Welcome to Your Vue.js Apccp"/>
   </div>
 </template>
 
